2f5772d9a78363c9500d1f64353c8d29c799bbd2
pranav <pramech.2@gmail.com>
2023-02-15 15:12:13 +0530

    First commit

---
+++ githubpractice/src/githubpractice/char_11042022.java
@@ -0,0 +1,34 @@
+package githubpractice;
+
+public class char_11042022 
+{
+  public static void main(String[] args) 
+  {
+	//  main method  body
+	  
+	  // array  declaration 
+	  
+	  char[] ar5=new  char[5];
+	  
+	  // array initialization  // index value = 5-1= 4...0,1,2,3,4...
+	  
+	  ar5[0]='A';
+	  ar5[1]='B';
+	  ar5[2]='c';
+	  ar5[3]='D';
+	  ar5[4]='E';
+	  
+	  // usage
+	  
+	  for (int i=0; i<=ar5.length-1; i++)
+	  {
+		  System.out.println(ar5[i]);
+	  }
+	  
+	  
+	  
+}
+	
+	
+
+}
